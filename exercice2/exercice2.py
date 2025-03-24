9014393151e24e630607556a6a0e7b27ae1d14ce
LeDuc <leandre.leduc38@gmail.com>
2025-03-24 17:28:03 +0100

    implémentation de la regression logistique à K classe

--- exercice2/exercice2.py
+++ exercice2/exercice2.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from display import affichage_fonction_erreur, affichage_fonction_precision
 
 #%% Introduction : Lecture des jeux de données fournis 
 def readdataset2d(fname):
@@ -14,13 +18,12 @@ def readdataset2d(fname):
     return np.array(X), T
 
 #%% Fonction convertit de la question 2
-def convertit(T: np.array, D: int) -> np.array:
-    new_T= []
-    for classification in T:
-        line_i = np.zeros(D)
-        line_i[classification - 1] = 1
-        new_T.append(line_i)
-    return np.array(new_T)
+def convertit(T: np.array, K: int) -> np.array:
+    new_T = np.zeros((len(T), K))  
+    for i, classification in enumerate(T):
+            new_T[i, classification - 1] = 1 
+    return new_T
+
 # print(convertit(np.array([1,2]), 2))
 
 def softmax(A: np.array) -> np.array:
@@ -33,24 +36,76 @@ def predit_proba(X: np.array, W: np.array, b: np.array):
     return softmax( X.dot(W) + b)
 
 # idée -> parcourir la ligne associé à Xi dans Y et voir l'indice de la plus grande valeur et ensuite convertir comme avec T_train
-def predit_classe(Y: np.array) -> np.array:
+def predit_classe(Y: np.array, K: int) -> np.array:
     prediction_index = np.argmax(Y, axis = 1) + 1
-    return convertit(prediction_index, D)
+    return convertit(prediction_index, K)
 
 
-    
+def initialise(D: int, K: int) -> tuple[np.array, np.array, np.array, np.array]:    
+    W = np.random.uniform(-2, 2, size = (D, K))
+    b = np.random.randn(1, K)
+    Y = predit_proba(X_train, W, b)
+    C = predit_classe(Y, K)
+    return W, b, Y, C
+
+def cross_entropy(Y: np.array, T: np.array):
+    N, K = Y.shape
+    J = 0
+    for n in range(N):
+        for k in range(K):
+            if T[n, k] == 1:
+                if np.log(Y[n, k]) == 0.0:
+                    continue
+                else :
+                    J -= T[n, k]*np.log(Y[n, k])
+    return J
+
+def updateWb(W: np.array, b: np.array,X: np.array, Y: np.array, T: np.array,lr: float):
+    W -= lr*(X.transpose()).dot((Y-T))
+    for j in range(0, K):
+        grad = 0
+        for n in range(N):
+            grad += np.sum(Y - T, axis = 1)[n]
+        b[0, j] -= lr*grad
+
+def taux_precision(C: np.array, T: np.array):
+    N = T.shape[0]
+    well_classified = 0
+    for i in range(N):
+        if C[i].all == T[i].all:
+            well_classified += 1
+    return well_classified*100/N
+
+def regression_logistique(W, b, X, Y, T, lr=0.1, nb_iter=1000, int_affiche=100, quiet = False ):
+    suite_erreur = [cross_entropy(Y,T)]
+    suite_precision = [taux_precision(predit_classe(Y, K), T)]
+    for i in range(nb_iter):
+        updateWb(W,b,X,Y,T,lr)
+        Y[:] = predit_proba(X, W, b)
+        if i % int_affiche == 0:
+            erreur_iter = cross_entropy(Y, T)
+            precision_iter = taux_precision(predit_classe(Y, K), T)
+            if not quiet:
+                print("Erreur cross_entropy a l'iteration ", i+1 ," : " , erreur_iter)
+            suite_erreur.append(erreur_iter)
+            suite_precision.append(precision_iter)
+    return suite_erreur
+
 
 
 
 #%% Import du jeu de données : probleme à 4 classes
 X_train, T_train = readdataset2d("probleme_4_classes")
 N, D = X_train.shape
-
+K = 4
 # Pour la visualisation, on garde T_train sous sa forme originelle
 plt.scatter(X_train[:,0], X_train[:,1], c=T_train, s = 30)
 # plt.show()
-
-
+W, b, Y_train, C_train_init = initialise(D, K)
+T_train = convertit(T_train, K)
+W_init = W.copy()
+b_init = b.copy()
+affichage_fonction_erreur(regression_logistique(W, b, X_train, Y_train, T_train, lr=0.01))
 # #%% Import du jeu de données : probleme à 5 classes
 # X_train, T_train = readdataset2d("probleme_5_classes")
 # N, D = X_train.shape
@@ -62,6 +117,7 @@ plt.scatter(X_train[:,0], X_train[:,1], c=T_train, s = 30)
 #%% Import du jeu de données : probleme à 6 classes
 X_train, T_train = readdataset2d("probleme_6_classes")
 N, D = X_train.shape
+K = 6
 
 # Pour la visualisation, on garde T_train sous sa forme originelle
 plt.scatter(X_train[:,0], X_train[:,1], c=T_train, s = 30)
